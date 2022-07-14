2c6362fee6742459e13dd4fa3545dcef661026e7
danielgchen <daniel.g.chen@gmail.com>
2022-07-14 15:10:45 -0700

    add in initial files

---
+++ cell.py
@@ -0,0 +1,7 @@
+# define the cell class
+# initialize the cell at a random location
+# initalize the cell with a random genome
+# define a method to move the cell
+# define a method for the cell to eat a piece of food
+# define a method for the cell to die
+# define a method for the cell to divide
